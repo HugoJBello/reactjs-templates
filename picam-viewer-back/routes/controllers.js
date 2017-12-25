3846b7bbd2092fd695ae7e60c839ea3cc3e10e82
HugoJBello <hugo.bello.gu@gmail.com>
2017-12-25 17:30:59 +0100

    7

--- picam-viewer-back/routes/controllers.js
+++ picam-viewer-back/routes/controllers.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var fs = require('fs');
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -25,5 +26,25 @@ router.get('/get_list_images/:limit', function(req, res, next) {
       console.log(result);
     });
 });
+
+router.get('/images_base64/:limit', function(req, res, next) {
+    con.query("SELECT * FROM image order by date_taken limit " + req.params.limit, function (err, result, fields) {
+      if (err) throw err;
+      for (var i=0;i<result.length;i++){
+      //  console.log(result[i].path);
+        result[i].base64 = base64_encode(result[i].path);
+      //  console.log(result[i].base64);
+      }
+      res.json(result);
+    });
+});
+
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
 
 module.exports = router;
