06786b28852d1eec16a022b07cb405ea81e38f3a
Hugo José Bello Gutiérrez <hjbg@gmv.es>
2018-01-11 17:58:43 +0100

    new endpoints

--- picam-viewer-back/routes/imageQueryPagedSearch.js
+++ picam-viewer-back/routes/imageQueryPagedSearch.js
@@ -25,7 +25,7 @@ router.get('/images_base64_date_paged/day=:day/page=:page', function(req, res) {
         con.query('SELECT count(*) FROM image where date_taken = "'+ req.params.day + '"', function (err, result, fields) {
           response.numberOfPages = Math.floor(result[0]['count(*)']/entriesPerPage)+1;
           response.numberOfItems = result[0]['count(*)'];
-          response.entriesPerPage = entriesPerPage;
+          response.entriesPerPage = entriesPerPage; 
           res.json(response);
         });
       });
@@ -69,24 +69,23 @@ router.get('/images_base64_paged_files/page=:page', function(req, res) {
       res.json(response);
     });
  } else {
-   res.json(null);
+   res.json(null); 
  }
 });
 
 router.get('/images_base64_date_paged_files/day=:day/page=:page', function(req, res) {
  if((req.params.day!==undefined) && (req.params.page!==undefined)){
-  console.log("----------" + req.param.day)
     limit = entriesPerPage;
     offset = entriesPerPage*(req.params.page-1);
     con.query('SELECT * FROM image where date_taken LIKE  "'+ req.params.day +'%"  and id > ' + offset + " limit " + limit, function (err, result, fields) {
+      if (err) throw err;
       for (var i=0;i<result.length;i++){
         result[i].base64 = base64_encode(result[i].path);
       }
       var response = result;
-      res.json(response);
+      res.json(response); 
     });
  } else {
-  console.log("----------" + req.params.day)
 
    res.json(null);
  }
@@ -101,6 +100,30 @@ router.get('/images_base64_parameters/', function(req, res) {
       });
 });
 
+router.get('/image_recorded/:filename', function(req, res) { 
+  console.log("---------");  
+  con.query('SELECT * FROM image where filename =  "'+ req.params.filename+'" limit 1', function (err, result, fields) {
+      if (err) throw err;    
+      try {
+        console.log(result[0]);  
+
+        var file = result[0].path;
+        // read binary data
+        var bitmap = fs.readFileSync(file); 
+        console.log("---------");  
+
+        console.log(file); 
+        // convert binary data to base64 encoded string
+        res.contentType('image/jpeg');
+        res.end(bitmap,"binary");
+      } catch (err) {
+        return null;
+      }      
+    });
+    
+});
+
+
 router.get('/images_base64_parameters_date/day=:day', function(req, res) { 
   con.query('SELECT count(*) FROM image where date_taken LIKE "'+ req.params.day + '"%', function (err, result, fields) {
     response.numberOfPages = Math.floor(result[0]['count(*)']/entriesPerPage)+1;
@@ -114,7 +137,7 @@ router.get('/images_base64_parameters_date/day=:day', function(req, res) {
 function base64_encode(file) {
   try {
     // read binary data
-    var bitmap = fs.readFileSync(file);
+    var bitmap = fs.readFileSync(file); 
     // convert binary data to base64 encoded string
     return new Buffer(bitmap).toString('base64');
   } catch (err) {
